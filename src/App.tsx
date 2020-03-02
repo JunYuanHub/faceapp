fd31037000c2a2d4755f2b65e5b5757f98362dad
JunYuanHub <JunYuanHub@users.noreply.github.com>
2020-03-02 23:42:54 +0800

    _

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Iprops {
+  name:string
 }
-
+interface IState {
+  state:string
+}
+class App extends React.Component<Iprops,IState>{
+  state={
+    state:"123"
+  };
+  render(){
+    return(
+        <div>
+          <span>{this.props.name}</span>
+          <span>{this.state.state}</span>
+        </div>
+    )
+  }
+}
 export default App;
